203036a6c9525d37424376082ad764e26605b90b
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-08-28 12:50:14 +0300

    Rearrange script for maintanance

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 const myLibrary = [];
 
-function Book(title, author, pageCount, doneReading) {
-  this.title = title;
-  this.author = author;
-  this.pageCount = pageCount;
-  this.doneReading = doneReading;
-}
-
 const slaughterHouseFiveBook = new Book(
   "Slaughterhouse Five",
   "Kurt Vonnegut",
   288,
   false
 );
-
 const duneBook = new Book("Dune", "Frank Herbert", 896, false);
+
+function Book(title, author, pageCount, doneReading) {
+  this.title = title;
+  this.author = author;
+  this.pageCount = pageCount;
+  this.doneReading = doneReading;
+}
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
